83cab670a709fddcb407a7ff9554f93b2742fe36
ShrutiSharma77 <shruti.sharma.met21@itbhu.ac.in>
2023-06-10 13:48:23 +0530

    updated app

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 @app.route('/')
 def home():
@@ -31,46 +32,53 @@ def process():
         j = 1
         average_values = []
         for value in averages_flattened:
-            average_values.append(f"{j}. {value}")
+            average_values.append(f"{value:.4f}")  # Remove the braces and format the value
             j = j + 1
 
-        range_value = int(request.form['range'])
+        session['avg_dia'] = avg_dia.tolist()
 
-        max_value = np.max(avg_dia)
-
-        total_grains = len(avg_dia)
-
-        rounded_value = math.ceil(max_value / 10) * 10
-
-        lower_bound = 0
-        upper_bound = rounded_value
-
-        x = []
-
-        while lower_bound < upper_bound:
-            range_label = f"{lower_bound}-{lower_bound+range_value}"
-            x.append(range_label)
-            lower_bound += range_value
-
-        x_new = x
-
-        num_ranges = upper_bound // range_value
-
-        count_array = [0] * num_ranges
-
-        for value in avg_dia:
-            lower_bound = int(value) // range_value * range_value
-            upper_bound_range = min(lower_bound + range_value, upper_bound)
-
-            range_index = lower_bound // range_value
-            count_array[range_index] += 1
-
-        y_new = np.array(count_array)
-        y = y_new / total_grains
-
-        return render_template('result.html', averages=average_values, x=x_new, y_new=y_new.tolist(), normalized_y=y.tolist(), range_value=range_value, max_value=max_value, total_grains=total_grains)
+        return render_template('result.html', averages=average_values, avg_dia=avg_dia)
+        
     
     return "No file uploaded."
+
+@app.route('/chart', methods=['POST'])
+def chart():
+    avg_dia = np.array(session['avg_dia'])
+    range_value = int(request.form['range'])
+    total_grains = len(avg_dia)
+
+    max_value = np.max(avg_dia)
+    rounded_value = math.ceil(max_value / 10) * 10
+
+    lower_bound = 0
+    upper_bound = rounded_value
+
+    x = []
+
+    while lower_bound < upper_bound:
+        range_label = f"{lower_bound}-{lower_bound+range_value}"
+        x.append(range_label)
+        lower_bound += range_value
+
+    x_new = x
+
+    num_ranges = upper_bound // range_value
+
+    count_array = [0] * num_ranges
+
+    for value in avg_dia:
+        lower_bound = int(value) // range_value * range_value
+        upper_bound_range = min(lower_bound + range_value, upper_bound)
+
+        range_index = lower_bound // range_value
+        count_array[range_index] += 1
+
+    y_new = np.array(count_array)
+    y = y_new / total_grains
+
+    return render_template('chart.html', x=x_new, y_new=y_new.tolist(), normalized_y=y.tolist(), range_value=range_value, max_value=max_value, total_grains=total_grains)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
